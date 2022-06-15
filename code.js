95ea4644542b4a0ccea46c3ce8aa2c7e21cf5cf7
Reagan701 <kingreggie702@gmail.com>
2022-06-15 16:06:18 +0200

    First Commit

---
+++ code.js
@@ -0,0 +1,8 @@
+let numbers = [1,3,12,4,5];
+
+document.write(`[${numbers}]`);
+document.write(`<br>`)
+document.write(`<br>`)
+document.write(Math.min(...numbers));
+document.write(`<br>`)
+document.write(Math.max(...numbers));
